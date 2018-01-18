7e177d1b79ef168325b1f140bc77c89071660bec
OneMacBoi <clarkston@mwc-032041.dhcp.missouri.edu>
2018-01-18 12:42:05 -0600

    Created Project

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by OneMacBoi on 1/18/18.
+//  Copyright © 2018 Stith, Clarkston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
